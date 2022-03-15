e51f76d53c89ba371c5416b1bd5f828606a5d9f6
cronos <bamlakdesalgn@gmail.com>
2022-03-15 18:59:52 +0300

    correcting task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,31 +4,25 @@
  * print_to_98 - print n to 98
  * @n: numberto start
  * Return: void
-*/
+ */
 void print_to_98(int n)
 {
 	if (n <= 98)
-		while (n <= 98)
+	{
+		for (; n <= 98; n++)
 		{
-			printf("%d", n);
-			if (n == 98)
-			{
-				printf("\n");
-				break;
-			}
-			printf(", ");
-			n++;
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
 		}
+	}
 	else
-		while (n >= 98)
+	{
+		for (; n >= 98; n--)
 		{
-			printf("%d", n);
-			if (n == 98)
-			{
-				printf("\n");
-				break;
-			}
-			printf(", ");
-			n--;
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
 		}
+	}
 }
