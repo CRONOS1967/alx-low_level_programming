d1020b3d8387d21a50e34923c7551269a9c7011a
cronos <bamlakdesalgn@gmail.com>
2022-03-15 19:11:50 +0300

    yet another method

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%d", n);
+			_putchar(_putchar(n));
 			_putchar(',');
 			_putchar(' ');
 		}
@@ -20,7 +20,7 @@ void print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d", n);
+			_putchar(_putchar(n));
 			_putchar(',');
 			_putchar(' ');
 		}
