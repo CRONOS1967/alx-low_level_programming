2915570c48a0ed563bc623509f1be7505ece7f97
cronos <bamlakdesalgn@gmail.com>
2022-03-21 18:37:37 +0300

    done with 4 taks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * print_rev - prints string in reverse followed by new line
+ * @s: string to be reversed
+ * Return- void
+ */
+void print_rev(char *s)
+{
+int i;
+
+for (i = _strlen(*s); i >= 0; i--)
+{
+_puts(*s);
+}
+_puts('\n');
+}
