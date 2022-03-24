b2fa66b9516efad53be3b712dcc03a29ac55d085
cronos <bamlakdesalgn@gmail.com>
2022-03-24 14:43:21 +0300

    fixing 3 tasks

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,24 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i;
+	int j;
 
-	int index = 0, dest_len = 0;
+	i = 0;
+	j = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
 
+	dest[i] = '\0';
 	return (dest);
 }
