9de6e3fa11ac9691adaae460a186997931c964ba
cronos <bamlakdesalgn@gmail.com>
2022-03-28 22:17:06 +0300

    done with madatory tasks

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s: string to be searched
+ * @c: char to be checked
+ *
+ * Return: pointer to the first occurence of c in s
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] != c && s[i] != '\0'; i++)
+		;
+
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
+}
