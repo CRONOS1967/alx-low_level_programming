35c75cc7dc1511583ce0d492c2888727c0610083
cronos <bamlakdesalgn@gmail.com>
2022-03-28 20:39:21 +0300

    finishing with task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills a memory block with a constant byte
+ * @s: address to memory block
+ * @b: char to be used
+ * @n: number of bytes to be used
+ *
+ * Return: pointer to the memory block
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+i = 0;
+
+while (i < n)
+{
+s[i] = b;
+}
+return (s);
+}
