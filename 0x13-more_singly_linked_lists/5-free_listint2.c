9ed46c2cf2da4349a990ada03b7f456f5b74ee8f
cronos <bamlakdesalgn@gmail.com>
2022-04-26 20:47:24 +0300

    fixing issues

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,26 +1,21 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- *free_listint2 - frees a linked list
- *@head: pointer to the head of the list
- *
- *Return: void
- */
+ * free_listint2 - Frees a list.
+ * @head: Address of the first node of a list.
+ **/
 
 void free_listint2(listint_t **head)
 {
-	listint_t *cursor;
-	listint_t **temp = head;
+	listint_t *temp;
 
-	if (temp != NULL)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		while (*head != NULL)
-		{
-			cursor = *head;
-			free(cursor);
-			*head = (*head)->next;
-		}
-
-		*temp = NULL;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 }
